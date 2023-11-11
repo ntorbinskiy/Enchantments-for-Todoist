f495963d6d37c6ce68e624e59dee4099416f9996
Nikita Torbinskiy <n.torbinskiy@gmail.com>
2023-11-11 20:15:17 +0200

    Add second test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -24,7 +24,7 @@ beforeEach(async () => {
 //   browser = undefined;
 // });
 
-test("Todoist opening", async () => {
+describe("Todoist health report:", async () => {
   if (!browser) {
     return;
   }
@@ -32,17 +32,27 @@ test("Todoist opening", async () => {
   const page = await browser.newPage();
 
   await page.goto("https://app.todoist.com/auth/login");
-  await page.waitForSelector("input");
 
-  const emailInput = await page.$("input[type='email']");
-  const passwordInput = await page.$("[type='password']");
-  const acceptButton = await page.$("[data-gtm-id='start-email-login']");
+  test("Todoist opening", async () => {
+    await page.waitForSelector("input");
 
-  if (!emailInput || !passwordInput || !acceptButton) {
-    throw new Error("Error");
-  }
+    const emailInput = await page.$("input[type='email']");
+    const passwordInput = await page.$("[type='password']");
+    const acceptButton = await page.$("[data-gtm-id='start-email-login']");
 
-  await emailInput.type("n.torbinskiy@icloud.com");
-  await passwordInput.type("NikPetrovich11");
-  await acceptButton.click();
+    if (!emailInput || !passwordInput || !acceptButton) {
+      throw new Error("Error");
+    }
+
+    await emailInput.type("n.torbinskiy@icloud.com");
+    await passwordInput.type("NikPetrovich11");
+    await acceptButton.click();
+    await page.waitForSelector(".simple_content");
+
+    const url = await page.url();
+
+    expect(url).toContain("/today");
+  }, 15000);
+
+  test("Unknown estimates", async () => {});
 });
