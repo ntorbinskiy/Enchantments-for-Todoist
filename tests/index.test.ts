d88595c99ccec96d5b73f0e142b8729f8c4bb047
Nikita Torbinskiy <n.torbinskiy@gmail.com>
2023-12-07 19:57:37 +0200

    Rewrite tests structure

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,10 @@
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
 import path from "path";
 
 const pathToExtension = path.join(process.cwd(), "dist");
 
-let browser: undefined | Browser = undefined;
+let browser: undefined | Browser;
+let page: undefined | Page;
 
 beforeEach(async () => {
   browser = await puppeteer.launch({
@@ -13,46 +14,51 @@ beforeEach(async () => {
       `--load-extension=${pathToExtension}`,
     ],
   });
+
+  page = await browser.newPage();
+
+  await page.goto("https://app.todoist.com/auth/login");
 });
 
-// afterEach(async () => {
-//   if (!browser) {
-//     return;
-//   }
-
-//   await browser.close();
-//   browser = undefined;
-// });
-
-describe("Todoist health report:", async () => {
+afterAll(async () => {
   if (!browser) {
     return;
   }
 
-  const page = await browser.newPage();
-
-  await page.goto("https://app.todoist.com/auth/login");
-
-  test("Todoist opening", async () => {
-    await page.waitForSelector("input");
-
-    const emailInput = await page.$("input[type='email']");
-    const passwordInput = await page.$("[type='password']");
-    const acceptButton = await page.$("[data-gtm-id='start-email-login']");
-
-    if (!emailInput || !passwordInput || !acceptButton) {
-      throw new Error("Error");
-    }
-
-    await emailInput.type("n.torbinskiy@icloud.com");
-    await passwordInput.type("NikPetrovich11");
-    await acceptButton.click();
-    await page.waitForSelector(".simple_content");
-
-    const url = await page.url();
-
-    expect(url).toContain("/today");
-  }, 15000);
-
-  test("Unknown estimates", async () => {});
+  await browser.close();
+  browser = undefined;
 });
+
+const todoistOpeningTest = async (): Promise<void> => {
+  if (!browser || !page) {
+    throw new Error("Error, browser or page are undefined");
+  }
+
+  await page.waitForSelector("input");
+
+  const emailInput = await page.$("input[type='email']");
+  const passwordInput = await page.$("[type='password']");
+  const acceptButton = await page.$("[data-gtm-id='start-email-login']");
+
+  if (!emailInput || !passwordInput || !acceptButton) {
+    throw new Error("Error");
+  }
+
+  await emailInput.type("n.torbinskiy@icloud.com");
+  await passwordInput.type("NikPetrovich11");
+  await acceptButton.click();
+};
+
+test("Todoist opening", async () => {
+  todoistOpeningTest();
+
+  if (!browser || !page) {
+    throw new Error("Error, browser or page are undefined");
+  }
+
+  await page.waitForSelector(".simple_content");
+
+  const url = await page.url();
+
+  expect(url).toContain("/today");
+}, 15000);
